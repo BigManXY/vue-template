e9da88675761cec5014165e133f11690cab4027b
xiaoyin <xiaoyin>
2018-01-26 16:22:30 +0800

    vuex测试修改

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld',
+import HelloWorld from '../components/HelloWorld'
 import homePage from '../pages/homePage'
 
 Vue.use(Router)
