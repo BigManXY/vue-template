7e2ee0bf06d1c082a483fd533c382116ada7665b
xiaoyin <xiaoyin>
2018-01-29 15:57:30 +0800

    初始化路由

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,25 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import routeConfig from './route.config.json';
-// import store from '../store'
+import routeConfig from './route.config.json'
 var registerRoute = (routeConfig) => {
-  let route = [];
+  let route = []
   routeConfig.map(pages => {
     route.push({
       path: pages.path,
       name: pages.name !== undefined ? pages.name : null,
       redirect: pages.redirect !== undefined ? pages.redirect : null,
-      component: require(`@/pages${pages.component}`).default,
+      component: require(`../${pages.component}`).default,
       meta: pages.meta !== undefined ? pages.meta : null,
       children: pages.children !== undefined ? registerRoute(pages.children) : null
     })
-  });
+  })
 
-  return route;
-};
+  return route
+}
 
-var route = registerRoute(routeConfig);
+var route = registerRoute(routeConfig)
 
 Vue.use(Router)
 
@@ -38,4 +37,3 @@ const routeInstance = new Router({
 // })
 
 export default routeInstance
-
