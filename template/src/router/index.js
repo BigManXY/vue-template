37104ddcd8759aa51f7a31454353258926f9fdf1
xiaoyin <xiaoyin>
2018-01-29 15:48:04 +0800

    初始化路由

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,21 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
-import homePage from '../pages/homePage'
+
+import routeConfig from './route.config.json';
+// import store from '../store'
+var registerRoute = (routeConfig) => {
+  let route = [];
+  routeConfig.map(pages => {
+    route.push({
+      path: pages.path,
+      name: pages.name !== undefined ? pages.name : null,
+      redirect: pages.redirect !== undefined ? pages.redirect : null,
+      component: require(`@/pages${pages.component}`).default,
+      meta: pages.meta !== undefined ? pages.meta : null,
+      children: pages.children !== undefined ? registerRoute(pages.children) : null
+    })
+  });
+
+  return route;
+};
+
+var route = registerRoute(routeConfig);
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/home',
-      name: 'homePage',
-      component: homePage
-    }
-  ]
+const routeInstance = new Router({
+  routes: route
 })
+
+// routeInstance.beforeEach((to, from, next) => {
+//   if (to.name !== 'login') {
+//     let userAccountId = store.state.userAccountId
+//     if (userAccountId === null) {
+//       routeInstance.replace({name: 'login'})
+//     }
+//   }
+//   next()
+// })
+
+export default routeInstance
+
